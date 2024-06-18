deadace16d0e9a425fd1ba4cf104781dfa45c108
chp320 <chp320@naver.com>
2024-06-18 23:08:14 +0900

    Fragment 기능 사용

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* 최초 내용 */
 // import logo from './logo.svg';
 // import './App.css';
 
@@ -24,14 +25,30 @@
 
 // export default App;
 
-import React from 'react';
 
+/* App 컴포넌트 함수 내용 수정 */
+// import React from 'react';
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>리액트 안녕!</h1>
+//       <h2>잘 작동하니?</h2>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+/* div 요소로 묶지 않고 Fragment 기능 사용 (리액트 v16 이상부터 적용된 기능) */
+import React, {Fragment} from "react";
 function App() {
   return (
-    <div>
-      <h1>리액트 안녕!</h1>
-      <h2>잘 작동하니?</h2>
-    </div>
+    <Fragment>
+      <h1>리액트 안녕?!</h1>
+      <h2>잘 작동하니?!</h2>
+    </Fragment>
   );
 }
 
